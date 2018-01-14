7db3b89ff53a303ff1b6176f1585cdc02cdb821d
yosukeueda33 <iwalkedalongthewalllikaeninjaa@gmail.com>
2018-01-14 12:58:47 +0900

    base files

---
+++ SwiftUrlAccessTimeout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftUrlAccessTimeout
+//
+//  Created by 上田陽介 on 2018/01/14.
+//  Copyright © 2018年 上田陽介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
