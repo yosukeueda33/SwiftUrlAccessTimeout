e960972ee5dac0ffd8ab9ee689d9750fdc1c1b26
yosukeueda33 <iwalkedalongthewalllikaeninjaa@gmail.com>
2018-01-14 13:39:33 +0900

    code of access dsired url

--- SwiftUrlAccessTimeout/ViewController.swift
+++ SwiftUrlAccessTimeout/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, URLSessionDataDelegate {
+    @IBOutlet weak var urlTextbox: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func accessButtonPushed(_ sender: Any) {
+        messageLabel.text = "accessing..."
+        
+        let myConfig = URLSessionConfiguration.default
+        myConfig.timeoutIntervalForRequest = 10 //the unit is second. 
+        let mySession = Foundation.URLSession(configuration: myConfig, delegate:self, delegateQueue: nil)
+        let myUrl:URL = URL(string: urlTextbox.text!)!
+        debugPrint(myUrl)
+        let myTask:URLSessionDataTask = mySession.dataTask(with: myUrl)
+        
+        myTask.resume()
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        DispatchQueue.main.async{
+            self.messageLabel.text = "succeeded."
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError: Error?){
+        DispatchQueue.main.async{
+            if (didCompleteWithError != nil){
+                self.messageLabel.text = "error."
+            }
+        }
+    }
 }
 
